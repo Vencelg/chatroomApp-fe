057aa7397716a9a41c3772ffdeaa81654de8f950
vencelg <gazdavaclav@gmail.com>
2022-06-12 11:29:28 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  // @ts-nocheck
+  import { onMount } from "svelte";
+import ChatroomBox from "./components/ChatroomBox.svelte";
+import ChatroomsPanel from "./components/ChatroomsPanel.svelte";
+
+  //const helloChannel = window.Echo.channel("channel-hello");
+  onMount(() => {
+    /* helloChannel.subscribed(() => {
+      console.log("subbed");
+    });
+
+    helloChannel.listen(".hello", (e) => {
+      console.log(e);
+    }); */
+  });
+</script>
+
+<main>
+  <div class="container">
+    <ChatroomsPanel />
+    <ChatroomBox />
+  </div>
+</main>
---
+++ src/main.js
@@ -0,0 +1,22 @@
+// @ts-nocheck
+import App from './App.svelte'
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
+
+window.Pusher = Pusher;
+
+// @ts-ignore
+  window.Echo = new Echo({
+  broadcaster: 'pusher',
+  key: "local",
+  wsHost: "127.0.0.1",
+  wsPort: 6001,
+  forceTLS: false,
+  disableStats: true
+});
+
+const app = new App({
+  target: document.getElementById('app')
+})
+
+export default app
